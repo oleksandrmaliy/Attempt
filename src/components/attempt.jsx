0e493babedb2e40f349aa364e09e2488efeb2776
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-08 11:37:07 +0200

    tailwind

---
+++ src/components/attempt.jsx
@@ -0,0 +1,40 @@
+const Attempt = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A atque
+            perspiciatis quisquam ipsa nemo molestiae quis corporis accusamus
+            illum deleniti odit natus architecto veritatis fugit in blanditiis,
+            ex modi dolore. Harum facilis ab deserunt perspiciatis doloremque
+            vitae autem magni, minima omnis officiis provident modi eius iusto
+            iste non possimus soluta, cum dignissimos in tempora voluptatum!
+            Asperiores incidunt suscipit illum culpa?
+          </p>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum,
+            laboriosam rem unde facere eaque excepturi sequi fugit tenetur modi,
+            quos dolorum! Molestias illum laborum facere dolorem alias. Ad,
+            dolorum. Non quaerat consequatur fugiat dicta tempore iure
+            consequuntur numquam, quas, tenetur voluptatum ratione cum explicabo
+            enim itaque eius debitis qui eligendi veritatis, consectetur
+            officiis quia. Voluptas perspiciatis mollitia doloremque aut.
+            Praesentium autem rerum provident iure maxime dolorem soluta,
+            accusamus aspernatur dolore consectetur possimus ipsum assumenda
+            alias architecto qui explicabo voluptates voluptatibus magni iusto
+            earum omnis cumque reprehenderit. Consequuntur, perspiciatis nulla.
+            Laudantium, esse obcaecati magni magnam vero reprehenderit corporis,
+            qui quia explicabo dolor nemo repudiandae, fuga minus libero
+            molestias? Iste inventore corrupti sint nobis quo ab nesciunt minus
+            enim recusandae quod?
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Attempt;
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        vibes: ['Great Vibes', 'cursive'],
+        poiret: ['Poiret One', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        redcolor: '#D88E73',
+        greencolor: '#2E936A',
+        bluecolor: '#81BEC5',
+        forevercolor: '#93D1C1',
+        backcolor: '#D5DCDE',
+        browncolor: '#D5DCDE',
+        yellowcolor: '#fef08a',
+      },
+      boxShadow: {
+        'custom-green': '5px 5px 10px #22c55e',
+        'all-yellow': '0 0 16px 16px #fef08a',
+        'all-green': '0 0 16px 8px #2E936A',
+      },
+      container: {
+        center: 'true', // Центрує контейнер
+        // padding: "1rem", // Додає внутрішні відступи
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
