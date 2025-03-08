d0c8ecf00c7744312acdcd8f33d204eb97335d7f
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-08 12:46:21 +0200

    two grids

--- src/components/attempt.jsx
+++ src/components/attempt.jsx
@@ -2,7 +2,39 @@ const Attempt = () => {
   return (
     <>
       <div className="container">
-        <div className="my-8 grid grid-cols-1 gap-16 border-4 border-red-500 p-10 sm:grid-cols-3">
+        <div className="my-8 grid grid-cols-1 gap-16 border-4 p-10 sm:grid-cols-3 sm:border-red-500 md:border-amber-500 lg:border-green-500">
+          <div className="sm:bg-blue-500 md:bg-green-500">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A atque
+              perspiciatis quisquam ipsa nemo molestiae quis corporis accusamus
+              illum deleniti odit natus architecto veritatis fugit in
+              blanditiis, ex modi dolore. Harum facilis ab deserunt perspiciatis
+              doloremque vitae autem magni, minima omnis officiis provident modi
+              eius iusto iste non possimus soluta, cum dignissimos in tempora
+              voluptatum! Asperiores incidunt suscipit illum culpa?
+            </p>
+          </div>
+          <div className="sm:col-span-2 sm:bg-blue-500 md:bg-green-500">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              ipsum, laboriosam rem unde facere eaque excepturi sequi fugit
+              tenetur modi, quos dolorum! Molestias illum laborum facere dolorem
+              alias. Ad, dolorum. Non quaerat consequatur fugiat dicta tempore
+              iure consequuntur numquam, quas, tenetur voluptatum ratione cum
+              explicabo enim itaque eius debitis qui eligendi veritatis,
+              consectetur officiis quia. Voluptas perspiciatis mollitia
+              doloremque aut. Praesentium autem rerum provident iure maxime
+              dolorem soluta, accusamus aspernatur dolore consectetur possimus
+              ipsum assumenda alias architecto qui explicabo voluptates
+              voluptatibus magni iusto earum omnis cumque reprehenderit.
+              Consequuntur, perspiciatis nulla. Laudantium, esse obcaecati magni
+              magnam vero reprehenderit corporis, qui quia explicabo dolor nemo
+              repudiandae, fuga minus libero molestias? Iste inventore corrupti
+              sint nobis quo ab nesciunt minus enim recusandae quod?
+            </p>
+          </div>
+        </div>
+        <div className="my-8 grid grid-cols-1 gap-16 border-4 border-red-500 p-10 md:grid-cols-3">
           <div className="md:bg-blue-500">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. A atque
@@ -14,7 +46,7 @@ const Attempt = () => {
               voluptatum! Asperiores incidunt suscipit illum culpa?
             </p>
           </div>
-          <div className="sm:col-span-2 md:bg-red-500">
+          <div className="sm:bg-blue-500 md:col-span-2 md:bg-green-500">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
               ipsum, laboriosam rem unde facere eaque excepturi sequi fugit
